2a8fef060700bfdcf395cee684d5ce4ad5f8e941
Vaidas Pilkauskas <vaidas.pilkauskas@gmail.com>
2017-01-02 21:22:57 +0200

    Rename field

--- core/src/main/java/com/github/liucijus/jinsist/expectations/Expectation.java
+++ core/src/main/java/com/github/liucijus/jinsist/expectations/Expectation.java
@@ -1,11 +1,11 @@
 package com.github.liucijus.jinsist.expectations;
 
 public class Expectation<MockType, ReturnType> {
-    private ExpectedInvocation<MockType> invocation;
+    private ExpectedInvocation<MockType> expectedInvocation;
     private final ReturnType result;
 
-    Expectation(ExpectedInvocation<MockType> invocation, ReturnType result) {
-        this.invocation = invocation;
+    Expectation(ExpectedInvocation<MockType> expectedInvocation, ReturnType result) {
+        this.expectedInvocation = expectedInvocation;
         this.result = result;
     }
 
@@ -14,13 +14,13 @@ public class Expectation<MockType, ReturnType> {
     }
 
     boolean isFor(Invocation invocation) {
-        return this.invocation.getInstance().equals(invocation.getInstance())
-                && this.invocation.getMethod().equals(invocation.getMethod())
-                && this.invocation.getArguments().matches(invocation.getArguments());
+        return this.expectedInvocation.getInstance().equals(invocation.getInstance())
+                && this.expectedInvocation.getMethod().equals(invocation.getMethod())
+                && this.expectedInvocation.getArguments().matches(invocation.getArguments());
     }
 
-    ExpectedInvocation getInvocation() {
-        return invocation;
+    ExpectedInvocation getExpectedInvocation() {
+        return expectedInvocation;
     }
 
     @Override
@@ -30,13 +30,13 @@ public class Expectation<MockType, ReturnType> {
 
         Expectation that = (Expectation) o;
 
-        return invocation.equals(that.invocation)
+        return expectedInvocation.equals(that.expectedInvocation)
                 && (result != null ? result.equals(that.result) : that.result == null);
     }
 
     @Override
     public int hashCode() {
-        int result1 = invocation.hashCode();
+        int result1 = expectedInvocation.hashCode();
         result1 = 31 * result1 + (result != null ? result.hashCode() : 0);
         return result1;
     }
--- core/src/main/java/com/github/liucijus/jinsist/expectations/UnexpectedInvocation.java
+++ core/src/main/java/com/github/liucijus/jinsist/expectations/UnexpectedInvocation.java
@@ -2,6 +2,6 @@ package com.github.liucijus.jinsist.expectations;
 
 public class UnexpectedInvocation extends RuntimeException {
     UnexpectedInvocation(Expectation expectation, Invocation invocation) {
-        super("Expected " + expectation.getInvocation() + ", but was " + invocation);
+        super("Expected " + expectation.getExpectedInvocation() + ", but was " + invocation);
     }
 }
